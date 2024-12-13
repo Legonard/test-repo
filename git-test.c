68269140a82f81c82d4b42cd23b7c39fd2508c5d
Legonard <Leonard.Marx@babyfluesterer.com>
2024-12-13 17:30:29 +0100

    Test123

--- git-test.c
+++ git-test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    float test = 1.838364748373658;
+    float test = 1.8398;
     printf("Test für ein Repository in Git\n\n");
     printf("%f\n", test);
     printf("Kann man das jetzt sehen?");
