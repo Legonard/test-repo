c05312d16e113ef004f45f66db8237308f38d879
Legonard <Leonard.Marx@babyfluesterer.com>
2024-12-13 11:07:49 +0100

    Erste Test Datei

---
+++ git-test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float test = 1.838364748373658;
+    printf("Test für ein Repository in Git\n\n");
+    printf("%f\n", test);
+    return 0;
+}
