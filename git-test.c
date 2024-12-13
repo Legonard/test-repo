5e77609282a4cc3f5c3f258808473c3726ff5a49
Legonard <Leonard.Marx@babyfluesterer.com>
2024-12-13 11:43:46 +0100

    Funktioniert das jetzt auch?

--- git-test.c
+++ git-test.c
@@ -5,5 +5,6 @@ int main() {
     float test = 1.838364748373658;
     printf("Test für ein Repository in Git\n\n");
     printf("%f\n", test);
+    printf("Kann man das jetzt sehen?");
     return 0;
 }
